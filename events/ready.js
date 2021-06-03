e010b13a746f8341db5a7e3627bea6398a019149
Joseph <71621973+joeyk710@users.noreply.github.com>
2021-06-03 19:14:04 -0400

    Add files via upload

--- events/ready.js
+++ events/ready.js
@@ -1,9 +1,14 @@
-  
 module.exports = async (client) => {
-  client.Ready = true;
-  client.user.setActivity(">help", { type: "STREAMING", url:"https://www.twitch.tv/captain_motchy" }).then(() => {
+  client.Ready = true, 
+  client.user.setPresence({
+    status: "online",  // You can show online, idle, and dnd
+    activity: {
+        name: "Music",  // The message shown
+        type: "LISTENING", // PLAYING, WATCHING, LISTENING, STREAMING,
+    }
+});
     client.Manager.init(client.user.id);
-    client.log("Successfully Logged in as " + client.user.tag);
-  });
-  client.RegisterSlashCommands();
+    client.log("Successfully Logged in as " + client.user.tag); // You can change the text if you want, but DO NOT REMOVE "client.user.tag"
+client.RegisterSlashCommands();
 };
+
